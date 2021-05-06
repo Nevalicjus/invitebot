a0510427d3f0af1e433f5172a7acdcf9e91bda9d
Nevalicjus <50320623+Nevalicjus@users.noreply.github.com>
2021-05-06 20:46:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ async def status_task():
         members = 0
         for guild in client.guilds:
             members += guild.member_count
-        await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=f"on {len(client.guilds)} guilds with {members} members total"))
+        await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=f"on {len(client.guilds)} guilds with {members} members"))
         await asyncio.sleep(20)
         await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="i!help | https://discord.gg/wsEU32a3ke"))
         await asyncio.sleep(20)
