4e19c0c68348b78b8ee1d29ea60b549254e1a494
Nevalicjus <50320623+Nevalicjus@users.noreply.github.com>
2021-08-16 13:24:29 +0200

    added status stats via cmd

--- cogs/owner.py
+++ cogs/owner.py
@@ -167,6 +167,17 @@ class Owner(commands.Cog):
         #           self.log(0, f"Guild {guild.id} had no configuration present")
         #           noconf_guild_ids.append(guild.id)
 
+    @commands.command
+    #
+    # Sends guilds stats
+    #
+    async def stats(self, ctx):
+        members = 0
+        for guild in client.guilds:
+            members += guild.member_count
+        await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=f"on {len(client.guilds)} guilds with {members} members"))
+        await ctx.send(constructResponseEmbedBase(f"I'm on {len(self.client.guilds)} with {members} members"
+
 
     def log(self, guild_id, log_msg: str):
         with open('main-config.json', 'r') as f:
